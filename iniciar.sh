a1f0447f3f46909416697383a7352cd2b1b88b91
ggzor <30713864+ggzor@users.noreply.github.com>
2021-08-18 12:32:22 -0500

    Actualizando script para especificar opcionalmente el directorio de trabajo

--- iniciar.sh
+++ iniciar.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+DIRECTORY="$(pwd)"
+
+if (( $# > 0 )); then
+  DIRECTORY=$1
+fi
+
+if [[ ! -d "$DIRECTORY" ]]; then
+  echo "El directorio no existe. :("
+  exit 1
+fi
+
 docker run --name php \
            --rm -it \
            -p 80:80 \
-           -v "$(pwd):/opt/lampp/htdocs" \
+           -v "$DIRECTORY:/opt/lampp/htdocs" \
            tomsik68/xampp:7
 
