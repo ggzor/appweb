6627379fdee87c0fc78051f115c53591a0276085
ggzor <30713864+ggzor@users.noreply.github.com>
2021-09-01 01:21:37 -0500

    Agregando toques finales

--- actividad4/agradecer.php
+++ actividad4/agradecer.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gracias por votar</title>
+    <link rel="stylesheet" href="./compartido.css" />
     <link rel="stylesheet" href="./agradecer.css" />
 </head>
 
--- actividad4/graficaVotos.php
+++ actividad4/graficaVotos.php
@@ -5,16 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Actividad 3</title>
+    <link rel="stylesheet" href="./compartido.css" />
+    <link rel="stylesheet" href="graficaVotos-base.css">
+    <link rel="stylesheet" href="graficaVotos.css">
 </head>
 
 <body>
     <h1>Conteo de votos</h1>
-    <hr>
+    <a href="votar.php">Votar</a>
+    <br>
+    <br>
     <?php
     $conn = new mysqli("localhost", "root", null, "votaciones");
     $resultados = $conn->query("
-        SELECT partido, COUNT(id_voto) as conteo 
-        FROM voto 
+        SELECT partido, COUNT(id_voto) as conteo
+        FROM voto
         GROUP BY partido");
 
     $contadores = [
@@ -24,12 +29,33 @@
         "MORENA" => 0,
     ];
 
+
     while ($row = $resultados->fetch_assoc()) {
         $contadores[$row['partido']] = intval($row['conteo']);
     }
 
-    var_dump($contadores);
+    $maximo = max($contadores);
+    $zero = $maximo === 0 ? 'data-zeroes' : '';
+
+    echo "
+<article class='grafica' $zero style='--data-max: $maximo;'>
+    ";
+
+    foreach ($contadores as $key => $value) {
+      echo "
+    <div class='bar' style='--data-count: $value;'></div>";
+    }
     ?>
+
+    <img width="40" src="./imagenes/pan.png">
+    <img width="40" src="./imagenes/pri.png">
+    <img width="40" src="./imagenes/prd.png">
+    <img width="40" src="./imagenes/morena.png">
+</article>
+
+<script>
+/* setTimeout(() => location.reload(), 2000); */
+</script>
 </body>
 
 </html>
