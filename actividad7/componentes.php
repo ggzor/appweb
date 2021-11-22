5f380ee0f765f64dc5cc92260e8d2b0001579281
ggzor <30713864+ggzor@users.noreply.github.com>
2021-11-22 15:13:22 -0600

    Updating details menu

--- actividad7/componentes.php
+++ actividad7/componentes.php
@@ -175,9 +175,9 @@ EOF;
 $m_details = function ($selected) {
   $cls = $selected ? "seleccionado" : "";
   return <<<EOF
-<a class="$cls" href="about.php">
+<a class="$cls" href="javascript:location.reload()">
   <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M16 6L18.29 8.29L13.41 13.17L9.41 9.17L2 16.59L3.41 18L9.41 12L13.41 16L19.71 9.71L22 
+    <path d="M16 6L18.29 8.29L13.41 13.17L9.41 9.17L2 16.59L3.41 18L9.41 12L13.41 16L19.71 9.71L22
              12V6H16Z"/>
   </svg>
   Detalles
